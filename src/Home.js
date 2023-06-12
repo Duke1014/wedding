2625528faebf0fcd11a11a356b742c590daf8914
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-06-11 21:01:33 -0400

    moving to laptop

---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <p>Duke and Mo</p>
+        <p>July 25, 2023</p>
+        <p>Oswego, NY</p>
+    </div>
+  )
+}
---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <span>
+                    Duke a la Mo
+                </span>
+                <ul>
+                    <li>
+                        Our Story
+                    </li>
+                    <li>
+                        Registry
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
