6100871426f6351fcec88b7e5815af69b3a00b84
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-06-12 08:32:20 -0400

    doomsday clock actually working now

--- src/Home.js
+++ src/Home.js
@@ -10,9 +10,13 @@ export default function Home() {
 
   const countdownCalc = () => {
     let wedding = new Date(2023, 6, 25)
+    let sign
     wedding.setHours(11)
     let today = new Date()
-    let calc = today - wedding
+    if (Math.sign(today - wedding) === -1) {
+      sign = '-'
+    }
+    let calc = Math.abs(today - wedding)
     let dayCalc = calc / 86400000 
     let days = Math.floor(dayCalc)
     let hourCalc = dayCalc - days
@@ -21,12 +25,7 @@ export default function Home() {
     let mins = minCalc * 60
     let secCalc = mins - Math.floor(mins)
     let secs = secCalc * 60
-    
-    // console.log(`Days: ${days}`)
-    // console.log(`Hours: ${Math.floor(hours)}`) 
-    // console.log(`Minutes: ${Math.floor(mins)}`)
-    // console.log(`Seconds: ${Math.floor(secs)}`)
-    setCountdown(`${days} days, ${Math.floor(hours)} hours, ${Math.floor(mins)} minutes, and ${Math.floor(secs)} seconds`) 
+    setCountdown(`${sign}${days} days, ${Math.floor(hours)} hours, ${Math.floor(mins)} minutes, and ${Math.floor(secs)} seconds`) 
   }
 
   return (
@@ -34,7 +33,7 @@ export default function Home() {
         <p>Duke and Mo</p>
         <p>July 25, 2023</p>
         <p>Oswego, NY</p>
-        <p>It has been {countdown} since Duke and Mo have been married!</p>
+        <p className = 'clock'>It has been {countdown} since Duke and Mo have been married!</p>
     </div>
   )
 }
