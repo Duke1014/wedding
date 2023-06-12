484c86a3ad24a4bee362bd66e0159370bd04e22b
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-06-12 09:35:36 -0400

    navbar work

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,23 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 export default function Navbar() {
+
+    const [menu, setMenu] = useState(false)
+
     return (
         <div>
             <nav>
-                <span>
+                <span className='title'>
                     Duke a la Mo
                 </span>
-                <ul>
-                    <li>
-                        Our Story
-                    </li>
-                    <li>
-                        Registry
-                    </li>
-                    <li>
-                        Contributors
-                    </li>
-                </ul>
+
+                <FontAwesomeIcon 
+                    className='menu-icon'
+                    icon={faBars} 
+                    style={{color: "#0069e7",}} 
+                    onClick={() => setMenu(!menu)}
+                />
+                {menu ? <>
+                    <ul className='menu'>
+                        <li>
+                            Our Story
+                        </li>
+                        <li>
+                            Registry
+                        </li>
+                        <li>
+                            Contributors
+                        </li>
+                    </ul>
+                </> : <></>}
             </nav>
         </div>
     )
