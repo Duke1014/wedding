9e98ca1feaea8c0d70f4ac531f8b882df470e174
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-07-08 12:56:29 -0400

    contributor stuff

--- src/Contributors.js
+++ src/Contributors.js
@@ -3,19 +3,42 @@ import React from 'react'
 import clipclocks from './img/clipclocks.jpg'
 import dragonflysrealm from './img/dragonflysrealm.jpg'
 
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Contributors() {
 
     const contributorImages = [
       dragonflysrealm,
-      clipclocks
+      clipclocks,
+      theartofwood,
+      elderearthwood,
+      lyndakavy,
+      jimhancock
     ]
 
     return (
         <div>
             <p className='font-view'>
-                A lot of you and our friends have contributed to making this day happen, and we would like to thank each and every one of you!
+                Our friends and family have contributed to making this day happen, and we would like to thank each and every one of you!
             </p>
+            <div className='carousel-container'>
+                <div className='carousel'>
+                    <div className='left-arrow'>
+                        <FontAwesomeIcon icon={faChevronLeft} />
+                    </div>
+                    <div className='img-container'>
 
+                    </div>
+                    <div className='right-arrow'>
+                        <FontAwesomeIcon icon={faChevronRight} />
+                    </div>
+                </div>
+                <div className='bottom-buttons'>
+
+                </div>
+            </div>
         </div>
     )
 }
