5e76460e3ab98331f2909d42f24db11b00a3a341
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-07-17 15:17:26 -0400

    mobile time

--- src/Registry.js
+++ src/Registry.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Registry() {
     return (
         <div>
-            <p>
+            <p className='font-view'>
                 While we don't have a real registry, the one thing we truly want is more experiences together. Some of our ideas was to go to Japan, Ireland, England, Disney World, the list goes on. Helping us fill our vacation jar would be more than enough!
                 <br /><br />
                 *venmo here*
