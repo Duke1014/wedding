32010dd11dae4a4696d2b17810af6ae791ca31ec
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-07-20 13:18:49 -0400

    venmo added

--- src/Registry.js
+++ src/Registry.js
@@ -6,7 +6,9 @@ export default function Registry() {
             <p className='font-view'>
                 While we don't have a real registry, the one thing we truly want is more experiences together. Some of our ideas was to go to Japan, Ireland, England, Disney World, the list goes on. Helping us fill our vacation jar would be more than enough!
                 <br /><br />
-                *venmo here*
+                <a href='https://account.venmo.com/u/Maureen-Fenninger'>
+                    Click here for VenMO! (haha get it?)
+                </a>
             </p>
         </div>
     )
